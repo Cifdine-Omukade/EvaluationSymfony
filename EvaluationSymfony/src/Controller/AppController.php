cb97792c2fd5bd5bb6726837ebaa5657e2a90b49
Your Name <Cireguig@gmail.com>
2020-03-20 17:21:12 +0100

    remonter au commit ou on demande de trier la page d'accueil pour la voir sinon ne marche pas dans ce commit

--- EvaluationSymfony/src/Controller/AppController.php
+++ EvaluationSymfony/src/Controller/AppController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Country;
 use App\Repository\CountryRepository;
+use App\Repository\StatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,11 +16,18 @@ class AppController extends AbstractController
     /**
      * @Route("", name="app_index",methods={"GET"})
      */
-    public function index(CountryRepository $countryRepository)
+    public function index(CountryRepository $countryRepository,StatRepository $statRepository)
     {
+        $country = $countryRepository->find($country);
         $countries = $countryRepository->AlphabeticalOrder();
+        
+        
+        $countryStat = $country->getStat($statRepository->MyFindBy($country));
         return $this->render('app/index.html.twig', [
-            'countries' => $countries
+            'countries' => $countries,
+            'stat' => $countryStat,
+            'country' => $country
+
         ]);
     }
 }
